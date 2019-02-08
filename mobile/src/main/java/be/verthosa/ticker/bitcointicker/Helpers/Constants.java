46b1d31db20c7526e567bdc8951594b49f43f4a0
ehb\kurt.bouche <kurt.bouche@student.ehb.be>
2019-02-08 14:49:44 +0100

    Cleanup

--- mobile/src/main/java/be/verthosa/ticker/bitcointicker/Helpers/Constants.java
+++ mobile/src/main/java/be/verthosa/ticker/bitcointicker/Helpers/Constants.java
@@ -3,5 +3,5 @@ package be.verthosa.ticker.bitcointicker.Helpers;
 public class Constants {
     // seconds.
     public static final int DEFAULT_PRICE_INTERVAL = 10;
-    public static final int DEFAULT_NEWS_INTERVAL = 30;
+    public static final int DEFAULT_NEWS_INTERVAL = 30; 
 }
